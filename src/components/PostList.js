e2c9798b3e185c1ec870c32f3717b70fff2e3c41
Sew-a <sevavetisyan97@gmail.com>
2021-03-04 14:20:54 +0400

    commit

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,12 +4,13 @@ import { fetchPosts } from "../actions";
 import UserHeader from "./UserHeader";
 
 class PostList extends Component {
+ 
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPosts()
   }
 
   renderList() {
-    return this.props.posts.map((post) => {
+     this.props.posts.state  && this.props.posts.state.map((post) => {
       return (
         <div className="item" key={post.id}>
           <i className="large middle aligned icon user" />
@@ -26,7 +27,23 @@ class PostList extends Component {
   }
 
   render() {
-    return <div className="ui relaxed divided list">{this.renderList()}</div>;
+    return <div className="ui relaxed divided list">{ 
+      this.props.posts.state  && this.props.posts.state.map((post) => {
+        console.log(post)
+        return (
+          <div className="item" key={post.id}>
+            <i className="large middle aligned icon user" />
+            <div className="content">
+              <div className="description">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </div>
+              <UserHeader userId={post.userId} />
+            </div>
+          </div>
+        )
+      })
+    }</div>;
   }
 }
 
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,7 +1,9 @@
 export default (state = [], action) => {
   switch (action.type) {
     case "FETCH_POSTS":
-      return action.payload;
+      return {
+        state: action.payload
+      };
     default:
       return action;
   }
