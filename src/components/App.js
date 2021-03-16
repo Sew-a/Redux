3378b6823aa10cc3dd684fe3bf564b923c53d67e
Sew-a <sevavetisyan97@gmail.com>
2021-03-16 16:19:47 +0400

    router was created

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StreamDelete";
 
-function App() {
+import Header from './Header';
+
+const App = () => {
     return (
         <div>
-            <h1>Streams</h1>
+           <Router>
+           <Header />
+            <div>
+             <Route path="/" exact component={StreamList} />
+             <Route path="/streams/new" component={StreamCreate} />
+             <Route path="/streams/edit" component={StreamEdit} />
+             <Route path="/streams/delete" component={StreamDelete} />
+             <Route path="/streams/show" component={StreamShow} />
+            </div>
+           </Router>
         </div>
     )
 }
